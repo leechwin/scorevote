be719a166c8042d939b2c31d5bc5d0252bf92dcb
changhyun1.lee <changhyun1.lee@samsung.com>
2014-08-01 15:22:56 +0900

    Ininital commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+var express = require('express');
+
+var app = express();
+app.set( 'port', process.env.PORT || 3000 );
+
+var server = http.createServer( function(request, response) {
+    fs.readFile('index.html', function (error, data) {
+	    response.writeHead(200, {'Content-Type': 'text/html' });
+		response.end(data);
+	});
+}).listen(app.get('port'), function () {
+        console.log('Server Running at http://172.21.111.44:8080');
+});
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+    socket.on('message', function (data) {
+	    io.sockets.emit('message',data);
+	});
+});
