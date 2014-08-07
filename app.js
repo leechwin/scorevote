bc021728883f5e47859243b8ff88c6d663d01fa7
changhyun1.lee <changhyun1.lee@samsung.com>
2014-08-07 22:34:38 +0900

    Fixed save contents

--- app.js
+++ app.js
@@ -16,9 +16,32 @@ var server = http.createServer( function(request, response) {
         console.log('Server Running at http://172.21.111.44:8080');
 });
 
+var date = null;
+var contents = '';
 var io = socketio.listen(server);
 io.sockets.on('connection', function (socket) {
     socket.on('message', function (data) {
-        io.sockets.emit('message',data);
+        if ( date == null ) {
+            date = new Date().getHours();
+        } else {
+            console.log("host time:" + date);
+            console.log("client time: " + data.date);
+
+            var diff = data.date - date;
+            console.log("diff time: " + diff);
+            if ( diff > 3 ) {
+                date = null;
+                contents = '';
+            }
+        }
+
+        // FIXME:: use database
+        contents += '<li>';
+        contents += '<h3>' + data.score + '</h3>';
+        contents += '<p>' + data.message + '</p>';
+        // contents += '<p>' + data.location + '</p>';
+        // contents += '<p>' + data.date + '</p>';
+        contents += '</li><br>';
+        io.sockets.emit('message', contents);
     });
 });
